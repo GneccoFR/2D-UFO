71acc3863d954f3270ba049f841390bdda8e218f
GneccoFR <GneccoFR@gmail.com>
2020-06-21 23:59:39 -0300

    Project Upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private int count;
+    public float speed;
+    private Rigidbody2D rb2d;
+    public Text countText;
+    public Text winText;
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement*speed);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+
+}
